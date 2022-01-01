6f960b594cbd5600e8c195878a3360e0a03bd99a
alireza kargar <imanesche@gmail.com>
2022-01-01 10:52:27 +0330

    add router

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("hello world")
+
+	// init Router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/", getBooks).Methods("GET")
+	http.ListenAndServe(":8000", r)
 }
